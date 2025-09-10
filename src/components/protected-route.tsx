58358b4049b69bbb7286c7aa1f92a2d2bbf887b7
Wagner Cardoso <wagnerdev01@gmail.com>
2025-09-09 22:48:38 -0300

    feat: add navbar

--- src/components/protected-route.tsx
+++ src/components/protected-route.tsx
@@ -1,4 +1,5 @@
 import { Navigate } from 'react-router-dom'
+import { Navbar } from './ui/navbar'
 
 interface ProtectedRouteProps {
   children: React.ReactNode
@@ -8,5 +9,10 @@ export function ProtectedRoute({ children }: ProtectedRouteProps) {
   //const isAuthenticated = localStorage.getItem('token') !== null
   const isAuthenticated = true
 
-  return isAuthenticated ? <>{children}</> : <Navigate to="/login" replace />
+  return isAuthenticated ? (
+    <main className="max-w-5xl mx-auto px-6 lg:px-0">
+      <Navbar />
+      {children}
+    </main>
+  ) : <Navigate to="/login" replace />
 }
